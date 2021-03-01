c9095b688c5069c2a156215d33228158c03fce5d
Sefa Burak OKCU <sefaburak.okcu@gmail.com>
2021-03-01 12:27:14 +0300

    [FEATURE] Make noisy labels and converted images parametric

--- src/gender_dataset.py
+++ src/gender_dataset.py
@@ -29,13 +29,15 @@ def convert_image_color(image, red=True):
 
 class GenderDataset(datasets.VisionDataset):
 
-  def __init__(self, root='./data', train=True, transform=None, target_transform=None, force=False, convert=True):
+  def __init__(self, root='./data', train=True, transform=None, target_transform=None,
+               force=False, convert=True, noisy_labels=False):
     super(GenderDataset, self).__init__(root, transform=transform,
                                 target_transform=target_transform)
     
     self.train = train
     self.force = force
     self.convert = convert
+    self.noisy_labels = noisy_labels
     
     self.prepare_converted_dataset()
     if self.train:
@@ -78,7 +80,7 @@ class GenderDataset(datasets.VisionDataset):
       return
 
     print('Preparing Converted Images for Gender Classification')
-    trainset= datasets.ImageFolder(data_dir)
+    trainset = datasets.ImageFolder(data_dir)
 
     trainset_1 = []
     trainset_2 = []
@@ -86,33 +88,36 @@ class GenderDataset(datasets.VisionDataset):
     for idx, (im, label) in enumerate(trainset):
       if idx % 10000 == 0:
         print(f'Converting image {idx}/{len(trainset)}')
-      im = im.resize((112,112))
+      # im = im.resize((112,112))
       
       im_array = np.array(im)
-
-      # Flip label with 25% probability
-      if np.random.uniform() < 0.25:
-        label = label ^ 1
-
-      # Color the image either red or green according to its possibly flipped label
-      color_red = label == 0
       
-      if self.train:
-          # Flip the color with a probability e that depends on the environment
-          if idx%2 == 0:
-            # 20% in the first training environment
-            if np.random.uniform() < 0.2:
-              color_red = not color_red
-          else:
-            # 10% in the first training environment
-            if np.random.uniform() < 0.1:
-              color_red = not color_red
-      else:
-        # 90% in the test environment
-        if np.random.uniform() < 0.9:
-          color_red = not color_red
-    
+      if self.noisy_labels:
+          # Flip label with 25% probability
+          if np.random.uniform() < 0.25:
+              label = label ^ 1
+            
       if self.convert:
+
+          # Color the image either red or green according to its possibly flipped label
+          color_red = label == 0
+          
+          if self.train:
+              # Flip the color with a probability e that depends on the environment
+              if idx%2 == 0:
+                # 20% in the first training environment
+                if np.random.uniform() < 0.2:
+                  color_red = not color_red
+              else:
+                # 10% in the first training environment
+                if np.random.uniform() < 0.1:
+                  color_red = not color_red
+          else:
+            # 90% in the test environment
+            if np.random.uniform() < 0.9:
+              color_red = not color_red
+        
+          
           colored_arr = convert_image_color(im_array, red=color_red)
       else:
           colored_arr = im_array
